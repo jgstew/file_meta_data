ec3f793235965d5edab24fbe67d15674cbd43ddc
JGStew <james@jgstew.com>
2022-03-26 14:15:29 -0400

    switching around single source version

--- versionfile.py
+++ versionfile.py
@@ -1,13 +1,16 @@
-__version__ = "0.1.0"
 
 import pyinstaller_versionfile
 
-package_name="file_metadata"
+import setuptools.config
+# https://www.programcreek.com/python/example/123119/setuptools.config.read_configuration
+config_parsed = setuptools.config.read_configuration("setup.cfg").get("metadata", {})
+package_name = config_parsed["name"]
+__version__ = config_parsed["version"]
 
 pyinstaller_versionfile.create_versionfile(
     output_file="versionfile.txt",
     version=__version__,
-    company_name="JGStew",
+    company_name=config_parsed["author"],
     file_description=package_name,
     internal_name=package_name,
     original_filename=package_name + ".exe",
